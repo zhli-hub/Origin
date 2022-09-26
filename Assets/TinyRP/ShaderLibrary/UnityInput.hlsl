d39e5d530ceaa10c99a141df9c72f8a98f51718e
lizhuohua <lizhuohua@ustc.edu>
2022-09-26 17:57:28 +0800

    [refs] Finish the srp batching.

---
+++ Assets/TinyRP/ShaderLibrary/UnityInput.hlsl
@@ -0,0 +1,15 @@
+#ifndef TINYRP_UNITY_INPUT_INLCUDE
+#define TINYRP_UNITY_INPUT_INLCUDE
+
+CBUFFER_START(UnityPerDraw)
+    float4x4 unity_ObjectToWorld;
+    float4x4 unity_WorldToObject;
+    float4 unity_LODFade;
+    real4 unity_WorldTransformParams;
+CBUFFER_END
+
+float4x4 unity_MatrixVP;
+float4x4 unity_MatrixV;
+float4x4 glstate_matrix_projection;
+
+#endif
